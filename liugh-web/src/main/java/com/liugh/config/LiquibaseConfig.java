85d257371363cfa5a4b01e8e0c353d3f34a6d636
王跃 <wangyue@kigoton.com>
2018-08-07 17:40:47 +0800

    no message

---
+++ liugh-web/src/main/java/com/liugh/config/LiquibaseConfig.java
@@ -0,0 +1,18 @@
+package com.liugh.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LiquibaseConfig {
+
+//    @Bean
+//    public SpringLiquibase liquibase(DataSource dataSource) {
+//        SpringLiquibase liquibase = new SpringLiquibase();
+//        liquibase.setDataSource(dataSource);
+//        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
+//        liquibase.setContexts("development,test,production");
+//        liquibase.setShouldRun(true);
+//        return liquibase;
+//    }
+
+}
