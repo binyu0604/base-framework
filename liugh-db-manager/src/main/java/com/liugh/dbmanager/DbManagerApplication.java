3ef828dfcbc24dc9d93632156ded4d8a407443c5
王跃 <wangyue@kigoton.com>
2018-08-08 17:43:50 +0800

    添加数据库版本管理模块

---
+++ liugh-db-manager/src/main/java/com/liugh/dbmanager/DbManagerApplication.java
@@ -0,0 +1,12 @@
+package com.liugh.dbmanager;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class DbManagerApplication {
+
+    //public static void main(String[] args) {
+    //    SpringApplication.run(DbManagerApplication.class , args);
+    //}
+}
